6ca6bfa893230596a7d8833cd8c5debcdc9518de
Sheila Kelley <spkelley26@gmail.com>
2020-06-25 08:03:14 -0700

    added requirements and gitignore

--- portfolio_django/portfolio_django/settings.py
+++ portfolio_django/portfolio_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vvt%ycv!_stwu8u&n1i^1luk)=^e9yj$(%lds@&5wo%uxc-08b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '159.89.131.22', 'localhost', 'sheilakelley.cloud']
 
 
 # Application definition
